79b814b0f169a320ac0a5b36822f52434e4045ab
Bhanu Pratap Singh <119663548+BhanuPratap0@users.noreply.github.com>
2024-02-08 22:34:18 +0530

    loader removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        loader: 'custom',
-        loaderFile: './loader.js',
+
         remotePatterns: [
             {
-
                 protocol: 'https',
                 hostname: "images.pexels.com"
             }
